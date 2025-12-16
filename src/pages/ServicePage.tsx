2eb59ac5821bd32a711901a5a76b9325aa9eb03e
Rvvshid04 <raashid.arq@gmail.com>
2025-12-16 20:00:29 +0530

    Updatd the services page and skill section

--- src/pages/ServicePage.tsx
+++ src/pages/ServicePage.tsx
@@ -3,35 +3,26 @@ const ServicePage = () =>{
     return(
         <div className="min-h-screen flex flex-col">
             <main className="flex-grow bg-gray-50 py-12 px-6">
-                <div className="max-w-5xl mx-auto">
+                <div className="max-w-6xl mx-auto">
                 <h1 className="text-4xl font-bold text-gray-800 mb-8 mt-8 text-center">
                     My Services<span className="text-indigo-500">.</span>
                 </h1>
 
-                <div className="grid gap-8 md:grid-cols-3">
+                <div className="grid gap-8 md:grid-cols-2">
                     {/* Tutoring */}
                     <div className="bg-white shadow-md rounded-2xl p-6 hover:shadow-lg transition">
                     <h2 className="text-2xl font-semibold mb-3 text-gray-700">
-                        Tutoring
+                        Online Private Tutoring
                     </h2>
                     <p className="text-gray-600 leading-relaxed">
-                        [Placecholder content] - Personalized tutoring in a range of subjects, tailored to your
-                        learning style. I focus on making concepts clear, practical, and
-                        easy to apply.
+                        One-to-one private tutoring held via Zoom or Google Meets focused on building strong programming and computer science fundamentals 
+                        through clear explanations, examples, and guided problem-solving.
                     </p>
-                    </div>
+                    <button className="inline-block bg-indigo-600 text-white font-semibold px-6 py-3 mt-4 rounded-xl shadow hover:bg-indigo-500 hover:text-white-600 transition">
+                        Learn More
+                    </button>
+                </div>
 
-                    {/* Marketing */}
-                    <div className="bg-white shadow-md rounded-2xl p-6 hover:shadow-lg transition">
-                    <h2 className="text-2xl font-semibold mb-3 text-gray-700">
-                        Marketing
-                    </h2>
-                    <p className="text-gray-600 leading-relaxed">
-                        [Placecholder content] - From crafting effective strategies to executing online campaigns,
-                        I help businesses grow their presence through social media,
-                        search, and email marketing.
-                    </p>
-                    </div>
 
                     {/* Software Development */}
                     <div className="bg-white shadow-md rounded-2xl p-6 hover:shadow-lg transition">
@@ -39,15 +30,17 @@ const ServicePage = () =>{
                         Software Development
                     </h2>
                     <p className="text-gray-600 leading-relaxed">
-                       [Placecholder content] - Building clean, efficient, and scalable software solutions—from
-                        web applications to technical problem-solving—tailored to your
-                        needs.
+                       I offer freelance software development for small projects, feature work, and bug fixes, with a 
+                       focus on clear communication, realistic scope, and maintainable solutions.
                     </p>
+                    <button className="inline-block bg-indigo-600 text-white font-semibold px-6 py-3 mt-10 rounded-xl shadow hover:bg-indigo-500 hover:text-white-600 transition">
+                        Learn More
+                    </button>
                 </div>
 
                 </div>
                     {/* Call to Action */}
-                    <div className="mt-16 bg-purple-600 text-white rounded-2xl shadow-lg p-10 text-center">
+                    <div className="mt-16 bg-indigo-600 text-white rounded-2xl shadow-lg p-10 text-center">
                         <h2 className="text-3xl font-bold mb-4">
                         Ready to Take the Next Step?
                         </h2>
